93d1d979501136103d82020c6386072d30ad3677
Harry <hs3061@columbia.edu>
2019-06-21 11:11:04 -0400

    starting up

---
+++ index.js
@@ -0,0 +1,51 @@
+// /index.js
+
+const server = require('server');
+var xss = require("xss");
+
+const {get, socket} = server.router;
+const { render } = server.reply;
+var id_to_username = {};
+
+const updateCounter = ctx => {
+	// console.log(Object.keys(ctx.io.sockets.sockets));
+	ctx.io.emit('count', Object.keys(ctx.io.sockets.sockets).length);
+};
+
+const handleDisconnect = ctx => {
+	// console.log("after DC");
+	// console.log(Object.keys(ctx.io.sockets.sockets));
+	ctx.io.emit('count', Object.keys(ctx.io.sockets.sockets).length);
+	const connected_set = new Set(Object.keys(ctx.io.sockets.sockets));
+	var users = []
+	for (const [id, username] of Object.entries(id_to_username)) {
+		if (!(id in connected_set)) {
+			users.push(username);
+			delete id_to_username[id];
+		}
+	} 
+	ctx.io.emit('leave', users);
+};
+
+const sendMessage = ctx => {
+	ctx.data.message = xss(ctx.data.message);
+	ctx.io.emit('message', ctx.data);
+};
+
+const sendJoin = ctx => {
+	id_to_username[ctx.data.id] = ctx.data.user;
+	ctx.io.emit('join', ctx.data);
+	// console.log(id_to_username)
+};
+
+
+server([
+	// for the inital render
+	get('/chat', ctx => render('chatroom.html')),
+	get('/', ctx => render('index.html')),
+	// Joining/leaving room
+	socket('connect', updateCounter),
+	socket('disconnect', handleDisconnect),
+	socket('message', sendMessage),
+	socket('join', sendJoin)
+]);
---
+++ public/javascript.js
@@ -0,0 +1,66 @@
+// /public/javascript.js
+
+
+// Connect to theh server-side websockets. but there's no server yet!
+var socket = io();
+
+// Get the current username from cookies
+socket.on('connect', async function(data) {
+	var user = await cookie.get('user');
+	while (!user) {
+		// no username set, so ask for it
+		user = await prompt('Choose a username:');
+		if (user) {
+			cookie.set('user', user);
+		}
+	}
+	const userInfo = {user : user, id : socket.id}
+	socket.emit('join', userInfo);	
+});
+
+
+// The user count. Can change when a person leaves or joins
+socket.on('count', function (data) {
+	$('.user-count').html(data);
+});
+
+// On receiving a message:
+// {user : 'username', message : 'text'}
+socket.on('message', function (data) {
+	$('.chat').append('<p><strong>' + data.user + '</strong>: ' + data.message + '</p>');
+	var objSection = document.getElementById("chatbox");
+	objSection.scrollTop = objSection.scrollHeight;
+});
+
+// On a user joining the chatroom
+socket.on('join', function (data) {
+	$('.chat').append('<p><strong>' + data.user + ' has joined.</strong></p>');
+});
+
+// On a user leaving the chatroom
+socket.on('leave', function (data) {
+	console.log(data)
+	for (var user of data) {
+		$('.chat').append('<p><strong>' + user + ' has left.</strong></p>');
+	}
+});
+
+// when form is submitted
+
+$('form').submit(function (e) {
+	//don't go through HTTP
+	e.preventDefault();
+
+	// get the message from user
+	var message = $(e.target).find('input').val();
+
+	// Send the message to the server
+	socket.emit('message', {
+		user: cookie.get('user') || 'Anonymous',
+		message: message
+	});
+
+	// Clear the input, focus for new message
+	e.target.reset();
+	$(e.target).find('input').focus();
+});
