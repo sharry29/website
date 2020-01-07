809c9879da64f9cb9c7ad438a970f162223a6b86
Harry <hs3061@columbia.edu>
2020-01-07 14:56:26 -0500

    adding photos/directory

--- index.js
+++ index.js
@@ -12,12 +12,10 @@ const { render } = server.reply
 
 server([
 	// for the inital render
+	get('/threenumbers', ctx => render('8-51-22.html')),
 	get('/chat', ctx => render('chatroom.html')),
 	get('/snake', ctx => render('snake.html')),
 	get('/', ctx => render('index.html')),
 	get('/cfa', ctx => render('apartments.html')),
 	get('/:junk', ctx => render('index.html')),
-	get('/8-51-22/:id', ctx => {
-		render('/index.html')
-	})
 ])
