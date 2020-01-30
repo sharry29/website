2ea4d5e557fae781850c221e64a862dd5d0ace42
Harry <hs3061@columbia.edu>
2020-01-30 18:18:50 -0500

    efott

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 // /index.js
 
 const server = require('server')
+const d3 = require('d3')
 var xss = require('xss')
 
 const { get, socket } = server.router
 const { render } = server.reply
 
-// function dailyViz(id) {
-// 	return render('index.html')
-// }
+function dailyViz() {
+	return render("8-51-22.html")
+}
 
 server([
 	// for the inital render
-	get('/threenumbers', ctx => render('8-51-22.html')),
+	get('/threenumbers', dailyViz),
 	get('/chat', ctx => render('chatroom.html')),
 	get('/snake', ctx => render('snake.html')),
 	get('/', ctx => render('index.html')),
