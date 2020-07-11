0136dfcc0765b8dbffc1dca0c0e0ac7bbc12fb64
Harry Smith <harryhas29@gmail.com>
2020-07-11 19:02:23 -0400

    little bit of the old redesign

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 // /index.js
 
-const server = require('server')
-const d3 = require('d3')
-var xss = require('xss')
+const server = require("server");
+// const d3 = require('d3')
+var xss = require("xss");
 
-const { get, socket } = server.router
-const { render } = server.reply
+const { get, socket } = server.router;
+const { render } = server.reply;
 
 function dailyViz() {
-	return render("8-51-22.html")
+  return render("8-51-22.html");
+}
+
+function showMyWork() {
+  return render("work.html");
 }
 
 server([
-	// for the inital render
-	get('/threenumbers', dailyViz),
-	get('/chat', ctx => render('chatroom.html')),
-	get('/snake', ctx => render('snake.html')),
-	get('/', ctx => render('index.html')),
-	get('/cfa', ctx => render('apartments.html')),
-])
+  // for the inital render
+  get("/threenumbers", dailyViz),
+  get("/chat", (ctx) => render("chatroom.html")),
+  get("/snake", (ctx) => render("snake.html")),
+  get("/", (ctx) => render("index.html")),
+  get("/cfa", (ctx) => render("apartments.html")),
+  get("/work", showMyWork),
+]);
